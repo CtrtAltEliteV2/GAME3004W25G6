54ed7c118be7f712e3419a3880c0b33d63c6b3a2
Reinaldo Roggiero <115185573+RSRFIPO@users.noreply.github.com>
2025-02-23 20:55:56 -0500

    created keymapping script for mapping actions

---
+++ Assets/Scripts/KeyMapperManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMapperManager : MonoBehaviour
+{
+    public static KeyMapperManager Instance;
+
+    private Dictionary<string, KeyCode> keyMappings;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadKeyMappings();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void LoadKeyMappings()
+    {
+        keyMappings = new Dictionary<string, KeyCode>
+        {
+            { "MoveForward", (KeyCode)PlayerPrefs.GetInt("MoveForward", (int)KeyCode.W) },
+            { "MoveBackward", (KeyCode)PlayerPrefs.GetInt("MoveBackward", (int)KeyCode.S) },
+            { "MoveLeft", (KeyCode)PlayerPrefs.GetInt("MoveLeft", (int)KeyCode.A) },
+            { "MoveRight", (KeyCode)PlayerPrefs.GetInt("MoveRight", (int)KeyCode.D) },
+            { "Jump", (KeyCode)PlayerPrefs.GetInt("Jump", (int)KeyCode.Space) },
+            { "UseItem", (KeyCode)PlayerPrefs.GetInt("UseItem", (int)KeyCode.E) },
+            { "PauseGame", (KeyCode)PlayerPrefs.GetInt("PauseGame", (int)KeyCode.Tab) }
+        };
+    }
+
+    public void SetKeyMapping(string action, KeyCode key)
+    {
+        keyMappings[action] = key;
+        PlayerPrefs.SetInt(action, (int)key);
+    }
+
+    public KeyCode GetKeyMapping(string action)
+    {
+        return keyMappings.ContainsKey(action) ? keyMappings[action] : KeyCode.None;
+    }
+}
